08d82e106844637d5640ba6e502e06d0a22055cf
Avendaño Ivan <iavend@cantv.com.ve>
2021-10-09 22:37:33 -0400

    Third Commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,5 @@
 
 
-// fetch('http://localhost:3000/weather?address=Caracas').then((response)=>{
-
-//       response.json().then((data)=>{ 
-//             if(data.error) {
-
-
-//                 console.log(data.error)
-//             } else {
-//             console.log(data.location)
-//             console.log(data.forecast) 
-//             }
-//       })
-
-// })
 
 const weatherForm = document.querySelector('form')
 const searchElement = weatherForm.querySelector('input')
